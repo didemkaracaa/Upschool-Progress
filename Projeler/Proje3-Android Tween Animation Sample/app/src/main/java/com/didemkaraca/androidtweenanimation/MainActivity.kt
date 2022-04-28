2b3ac71f98530a1d5dc81a57c27d456b891256a0
Didem Karaca <44816070+didemkaracaa@users.noreply.github.com>
2022-04-28 21:36:57 +0300

    AndroidTweenAnimation

---
+++ Projeler/Proje3-Android Tween Animation Sample/app/src/main/java/com/didemkaraca/androidtweenanimation/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.didemkaraca.androidtweenanimation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    private lateinit var imageView: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        imageView=findViewById(R.id.imageView)
+        button=findViewById(R.id.button)
+        val anim=AnimationUtils.loadAnimation(this,R.anim.anim)
+        button.setOnClickListener{
+            imageView.startAnimation(anim)
+        }
+    }
+}
